016c1629dcf9ed045d7ff4d1d0bd5bb519980adf
erripollo <pollone.enrico@gmail.com>
2021-06-15 16:55:35 +0200

    Create data structure for nav links in app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -9,7 +9,51 @@
 
 var app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    navLinks: [{
+      name: 'Home',
+      link: '#',
+      status: 'active',
+      type: 'link',
+      create: ''
+    }, {
+      name: 'About',
+      link: '#',
+      status: '',
+      type: 'link',
+      create: ''
+    }, {
+      name: 'Price',
+      link: '#',
+      status: '',
+      type: 'link',
+      create: ''
+    }, {
+      name: 'Courses',
+      link: '#',
+      status: '',
+      type: 'link',
+      create: 'new'
+    }, {
+      name: 'Locations',
+      link: '#',
+      status: '',
+      type: 'link',
+      create: ''
+    }, {
+      name: 'Blog',
+      link: '#',
+      status: '',
+      type: 'link',
+      create: ''
+    }, {
+      name: 'Book now',
+      link: '#',
+      status: '',
+      type: 'button',
+      create: ''
+    }]
+  },
   methods: {},
   mounted: function mounted() {}
 });
--- src/app.js
+++ src/app.js
@@ -1,7 +1,60 @@
 const app = new Vue ({
     el: '#app',
 
-    data:{},
+    data:{
+        navLinks: [
+            {
+                name: 'Home',
+                link: '#',
+                status: 'active',
+                type: 'link',
+                create: ''
+            },
+            {
+                name: 'About',
+                link: '#',
+                status: '',
+                type: 'link',
+                create: ''
+            },
+            {
+                name: 'Price',
+                link: '#',
+                status: '',
+                type: 'link',
+                create: ''
+            },
+            {
+                name: 'Courses',
+                link: '#',
+                status: '',
+                type: 'link',
+                create: 'new'
+            },
+            {
+                name: 'Locations',
+                link: '#',
+                status: '',
+                type: 'link',
+                create: ''
+            },
+            {
+                name: 'Blog',
+                link: '#',
+                status: '',
+                type: 'link',
+                create: ''
+            },
+            {
+                name: 'Book now',
+                link: '#',
+                status: '',
+                type: 'button',
+                create: ''
+            },
+
+        ]
+    },
 
     methods:{},
 
