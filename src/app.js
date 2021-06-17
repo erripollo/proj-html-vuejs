440f96ea8667d6772f83db99e9af891094d0a00a
erripollo <pollone.enrico@gmail.com>
2021-06-17 10:18:27 +0200

    create structure and style of card in rate section

--- src/app.js
+++ src/app.js
@@ -77,14 +77,14 @@ const app = new Vue ({
 
     mounted(){
         const numb = document.querySelector(".number");
-let counter = 0;
-setInterval(() => {
-  if(counter == 100 ){
-    clearInterval();
-  }else{
-    counter+=1;
-    numb.textContent = counter + "%";
-  }
-}, 80);
+        let counter = 0;
+        setInterval(() => {
+            if(counter == 100 ){
+                clearInterval();
+            }else{
+                counter+=1;
+                numb.textContent = counter + "%";
+            }
+        }, 80);
     }
 })
